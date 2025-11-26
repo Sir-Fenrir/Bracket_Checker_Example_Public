5e37abba19b6d9a98b089c19c075a6feeedf3df8
Sander ten Hoor <6397394+Sir-Fenrir@users.noreply.github.com>
2025-11-26 18:36:15 +0100

    Initial commit

---
+++ Bracket_Checker_Example_Public.Tests/BracketCheckerTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bracket_Checker_Example_Public.Tests;
+
+[TestClass]
+public class BracketCheckerTest
+{
+
+    [TestMethod]
+    public void TestCheckSuccess()
+    {
+        var result = BracketChecker.Check("(hello())");
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void TestCheckFail()
+    {
+        var result = BracketChecker.Check("(hello()");
+        Assert.IsFalse(result);
+    }
+}
---
+++ Bracket_Checker_Example_Public/BracketChecker.cs
@@ -0,0 +1,23 @@
+﻿namespace Bracket_Checker_Example_Public;
+
+public static class BracketChecker
+{
+    public static bool Check(string input)
+    {
+        var openCount = 0;
+        var closedCount = 0;
+        foreach (var c in input)
+        {
+            switch (c)
+            {
+                case '(':
+                    openCount++;
+                    break;
+                case ')':
+                    closedCount++;
+                    break;
+            }
+        }
+        return openCount == closedCount;
+    }
+}
